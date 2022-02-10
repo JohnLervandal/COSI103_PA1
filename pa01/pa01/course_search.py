17d232349954483cb2dcdc0090f9bd0b67af9fde
fritztheritz <fduverglas@brandeis.edu>
2022-02-10 13:06:06 -0500

    test push

--- pa01/pa01/course_search.py
+++ pa01/pa01/course_search.py
@@ -1,6 +1,7 @@
 '''
 course_search is a Python script using a terminal based menu to help
 students search for courses they might want to take at Brandeis
+testing
 '''
 
 from schedule import Schedule
