ba60154c72cc0d57819f5ad761b8598634a603a2
John Lervandal <55460368+JohnLervandal@users.noreply.github.com>
2022-02-17 18:37:02 -0500

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -26,30 +26,38 @@ class Schedule():
     def lastname(self,names):
         ''' lastname returns the courses by a particular instructor last name'''
         return Schedule([course for course in self.courses if course['instructor'][1] in names])
+
     def email(self,emails):
         ''' email returns the courses by a particular instructor email'''
         return Schedule([course for course in self.courses if course['instructor'][2] in emails])
+
     def term(self,terms):
         ''' email returns the courses in a list of term'''
         return Schedule([course for course in self.courses if course['term'] in terms])
+
     def enrolled(self,vals):
         ''' enrolled filters for enrollment numbers in the list of vals'''
         return Schedule([course for course in self.courses if course['enrolled'] in vals])
+
     def subject(self,subjects):
         ''' subject filters the courses by subject '''
         return Schedule([course for course in self.courses if course['subject'] in subjects])
+
     def sort(self,field):
         if field=='subject':
             return Schedule(sorted(self.courses, key= lambda course: course['subject']))
+            
         else:
             print("can't sort by "+str(field)+" yet")
             return self
     def limit(self, limit):
         ''' limit filters the courses by limit of class (created by Fritz)'''
         return Schedule(tuple([course for course in self.courses if course['limit'] == limit]))
+
     def title(self, phrase):
         '''search the course list to find phrase in title(created by Pedro)'''
         return Schedule([c for c in self.courses if phrase in c['name']])   
+        
     def description(self, phrase):
         '''search the course list to find phrase in description(created by Pedro)'''
         return Schedule([c for c in self.courses if phrase in c['description']])
