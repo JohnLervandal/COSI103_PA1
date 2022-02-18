b7c0413ccf6a27b7de50bb37d43e42fda5ae6981
panydulce <64445698+panydulce@users.noreply.github.com>
2022-02-18 00:14:35 -0500

    Revert "cleared some whitespace"
    This reverts commit e7ad39447c26ad319bad42a95c7cbaa16119bf22.

--- course_search.py
+++ course_search.py
@@ -94,9 +94,12 @@ def topmenu():
         print('\n'*3)
 
 def print_course(course):
-    '''print_course prints a brief description of the course '''
+    '''
+    print_course prints a brief description of the course 
+    '''
     print(course['subject'],course['coursenum'],course['section'],
           course['name'],course['term'],course['instructor'])
 
 if __name__ == '__main__':
     topmenu()
+
